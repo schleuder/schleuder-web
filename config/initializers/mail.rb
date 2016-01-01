b2f78d38a7affc889d0485fba7324be82d32df1f
paz <paz@nadir.org>
2016-01-02 00:11:15 +0100

    Mailer: sendmail_settings and smtp_settings from config.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,3 +1,7 @@
-Mail.defaults do
-  delivery_method Conf.delivery_method
+Rails.application.configure do
+  config.action_mailer.delivery_method = Conf.delivery_method
+  config.action_mailer.smtp_settings = Conf.smtp_settings
+  config.action_mailer.sendmail_settings = Conf.sendmail_settings
+  config.action_mailer.raise_delivery_errors = true
 end
+
