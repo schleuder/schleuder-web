9e72477c8b4559ff5e3b00524b9f074b14946dbc
paz <paz@nadir.org>
2016-01-04 10:56:08 +0100

    Allow deleting keys.

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -2,13 +2,10 @@ class KeysController < ApplicationController
   skip_load_and_authorize_resource
   skip_authorization_check
   before_filter :load_list_resource
+  before_filter :load_key, only: [:show, :destroy]
 
   def index
     @keys = @list.keys
-  end
-
-  def show
-    @key = @list.keys(params[:fingerprint])
   end
 
   def create
@@ -20,7 +17,20 @@ class KeysController < ApplicationController
     redirect_to @list, notice: "#{import_result.considered} key(s) considered for import. Result: #{import_result.imports.inspect}"
   end
 
+  def destroy
+    # destroy() doesn't read any params, but we need to give the list_id.
+    if Key.delete(@key.fingerprint, {list_id: @list.id})
+      redirect_to list_keys_path(@list), notice: "Key deleted: #{@key.fingerprint}"
+    else
+      redirect_to list_key_path(@list, @key), alert: "Deleting key failed: #{@key.errors.full_messages}"
+    end
+  end
+
   private
+
+  def load_key
+    @key = @list.keys(params[:fingerprint])
+  end
 
   def load_list_resource
     @list = List.find(params[:list_id])
