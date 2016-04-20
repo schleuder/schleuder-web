168c018008ed2980818658eedea3dd99e68df42f
paz <paz@nadir.org>
2016-04-20 14:44:16 +0200

    Fix setting delivery_method and {smtp,sendmail}_settings

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = Conf.delivery_method
-  config.action_mailer.smtp_settings = Conf.smtp_settings
-  config.action_mailer.sendmail_settings = Conf.sendmail_settings
+  config.action_mailer.delivery_method = Conf.delivery_method.to_sym
+  config.action_mailer.smtp_settings = Conf.smtp_settings.to_hash
+  config.action_mailer.sendmail_settings = Conf.sendmail_settings.to_hash
   config.action_mailer.raise_delivery_errors = true
 end
-
