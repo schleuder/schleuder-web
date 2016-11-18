76800d3b42c4018bdfc4764af5f094163c922056
paz <paz@nadir.org>
2016-11-18 22:44:22 +0100

    Don't use a global config in /etc, only config/webschleuder.yml.

--- app/models/conf.rb
+++ app/models/conf.rb
@@ -2,17 +2,7 @@ class Conf
   include ::Squire::Base
 
   def self.config_file
-    file_name = 'webschleuder.yml'
-    global_config = File.join(base_dir, file_name)
-    if File.readable?(global_config)
-      global_config
-    else
-      File.join(Rails.root, 'config', file_name)
-    end
-  end
-
-  def self.base_dir
-    ENV['SCHLEUDER_BASE_DIR'].try(:chomp, '/') || '/etc/schleuder'
+    File.join(Rails.root, 'config', 'webschleuder.yml')
   end
 
   squire.source self.config_file
