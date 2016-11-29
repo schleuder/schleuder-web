e24982f807b34dacda9be867d053ac6a3bff4726
paz <paz@nadir.org>
2016-11-29 12:39:35 +0100

    Check for tls_fingerprint only if use_tls is true.

--- config/initializers/check_mandatory_config.rb
+++ config/initializers/check_mandatory_config.rb
@@ -14,12 +14,14 @@ end
 #  fatal "remote_cert_file is set to a not readable file (in config/schleuder-web.yml)"
 #end
 
-if Conf.api.tls_fingerprint.blank?
-  fatal "Error: 'tls_fingerprint' is empty, can't verify connection to API without it (in config/schleuder-web.yml)."
-end
+if Conf.api_use_tls?
+  if Conf.api.tls_fingerprint.blank?
+    fatal "Error: 'tls_fingerprint' is empty but required if 'use_tls' is true (in config/schleuder-web.yml)."
+  end
 
-if Conf.api_use_tls? && Conf.api_key.blank?
-  fatal "Error: 'api_key' is empty but required if 'use_tls' is true (in config/schleuder-web.yml)."
+  if Conf.api_key.blank?
+    fatal "Error: 'api_key' is empty but required if 'use_tls' is true (in config/schleuder-web.yml)."
+  end
 end
 
 if Conf.api.host.blank?
