e4e700cc95e8174da07dab3aa8c9a860fc0eabd5
paz <paz@nadir.org>
2017-04-28 15:01:18 +0200

    Only assign selected delivery-settings.
    Assigning the others (e.g. `sendmail_settings`, if `delivery_method` is
    set to :smtp) appears to break delivery.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,13 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = Conf.delivery_method.to_sym
-  config.action_mailer.smtp_settings = Conf.smtp_settings.to_hash
-  config.action_mailer.sendmail_settings = Conf.sendmail_settings.to_hash
+  # Assign only the appropriate settings, assigning the others breaks delivery.
+  case Conf.delivery_method.to_sym
+  when :smtp
+    config.action_mailer.smtp_settings = Conf.smtp_settings.to_hash
+  when :sendmail
+    config.action_mailer.sendmail_settings = Conf.sendmail_settings.to_hash
+  when :file
+    config.action_mailer.file_settings = Conf.file_settings.to_hash
+  end
   config.action_mailer.raise_delivery_errors = true
 end
