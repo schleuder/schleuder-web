ec539ac7fa528977f186abcfe5984a1d7bb7f82e
Nina Siessegger <n.siessegger@zeromail.org>
2017-06-20 17:46:17 +0200

    Fix indentation

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,8 +1,8 @@
 class EmailValidator <  ActiveModel::EachValidator
   EMAIL_REGEXP = /\A.+@[[:alnum:]_.-]+\z/i
-    def validate_each(record, attribute, value)
-      unless value =~ EMAIL_REGEXP
-        record.errors[attribute] << (options[:message] || I18n.t("errors.invalid_email"))
-      end
+  def validate_each(record, attribute, value)
+    unless value =~ EMAIL_REGEXP
+      record.errors[attribute] << (options[:message] || I18n.t("errors.invalid_email"))
     end
+  end
 end
