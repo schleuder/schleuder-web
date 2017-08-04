f1d53027a8ddb8efc4ee6e8001f7504856deb254
paz <paz@nadir.org>
2017-08-04 10:30:04 +0200

    Fix checking only if API is required for production-env.
    Strange that the envs behave differently in this regard...

--- config/initializers/check_mandatory_config.rb
+++ config/initializers/check_mandatory_config.rb
@@ -1,33 +1,35 @@
-return if ! API_REQUIRED
+if API_REQUIRED
 
-def fatal(msg)
-  $stderr.puts "Error: #{msg}"
-  exit 1
-end
+  def fatal(msg)
+    $stderr.puts "Error: #{msg}"
+    exit 1
+  end
 
-# Disabled for now. Maybe we'll use this code some day, if we decide in favor of file-based verification.
-#
-#if Conf.api.remote_cert_file.blank?
-#  fatal "Can't verify connection to API without remote_cert_file! Set it in config/schleuder-web.yml"
-#end
-#
-#file = Pathname.new(Conf.api.remote_cert_file).expand_path
-#if ! file.readable?
-#  fatal "remote_cert_file is set to a not readable file (in config/schleuder-web.yml)"
-#end
+  # Disabled for now. Maybe we'll use this code some day, if we decide in favor of file-based verification.
+  #
+  #if Conf.api.remote_cert_file.blank?
+  #  fatal "Can't verify connection to API without remote_cert_file! Set it in config/schleuder-web.yml"
+  #end
+  #
+  #file = Pathname.new(Conf.api.remote_cert_file).expand_path
+  #if ! file.readable?
+  #  fatal "remote_cert_file is set to a not readable file (in config/schleuder-web.yml)"
+  #end
 
-if Conf.api.tls_fingerprint.blank?
-  fatal "Error: 'tls_fingerprint' is empty but required (in config/schleuder-web.yml)."
-end
+  if Conf.api.tls_fingerprint.blank?
+    fatal "Error: 'tls_fingerprint' is empty but required (in config/schleuder-web.yml)."
+  end
 
-if Conf.api_key.blank?
-  fatal "Error: 'api_key' is empty but required (in config/schleuder-web.yml)."
-end
+  if Conf.api_key.blank?
+    fatal "Error: 'api_key' is empty but required (in config/schleuder-web.yml)."
+  end
 
-if Conf.api.host.blank?
-  fatal "Error: 'host' is empty, can't connect (in #{ENV['SCHLEUDER_CONF_CONFIG']})."
-end
+  if Conf.api.host.blank?
+    fatal "Error: 'host' is empty, can't connect (in #{ENV['SCHLEUDER_CONF_CONFIG']})."
+  end
 
-if Conf.api.port.blank?
-  fatal "Error: 'port' is empty, can't connect (in #{ENV['SCHLEUDER_CONF_CONFIG']})."
+  if Conf.api.port.blank?
+    fatal "Error: 'port' is empty, can't connect (in #{ENV['SCHLEUDER_CONF_CONFIG']})."
+  end
+
 end
--- config/initializers/zzz_check_api_version.rb
+++ config/initializers/zzz_check_api_version.rb
@@ -1,29 +1,30 @@
-return if ! API_REQUIRED
+if API_REQUIRED
 
-REQUIRED_API_VERSION = '3.0'
+  REQUIRED_API_VERSION = '3.0'
 
-begin
-  found_api_version = Base.api_version
-  if Gem::Version.new(found_api_version) < Gem::Version.new(REQUIRED_API_VERSION)
-    $stderr.puts "
-      WARNING!
-      WARNING! The version of the running schleuder-api-daemon is too old!
-      WARNING!
-      WARNING! This schleuder-web will run into problems if you don't fix that!
-      WARNING! Required API-version: #{REQUIRED_API_VERSION}
-      WARNING! API-version running:  #{found_api_version}
-      WARNING!
-      ".gsub(/^\s*/, '')
+  begin
+    found_api_version = Base.api_version
+    if Gem::Version.new(found_api_version) < Gem::Version.new(REQUIRED_API_VERSION)
+      $stderr.puts "
+        WARNING!
+        WARNING! The version of the running schleuder-api-daemon is too old!
+        WARNING!
+        WARNING! This schleuder-web will run into problems if you don't fix that!
+        WARNING! Required API-version: #{REQUIRED_API_VERSION}
+        WARNING! API-version running:  #{found_api_version}
+        WARNING!
+        ".gsub(/^\s*/, '')
+    end
+
+  rescue Errno::ECONNREFUSED => exc
+      $stderr.puts "
+        WARNING!
+        WARNING! schleuder-api-daemon is not running!
+        WARNING! '#{exc}'
+        WARNING!
+        WARNING! This schleuder-web will be very useless without schleuder-api-daemon!
+        WARNING!
+        ".gsub(/^\s*/, '')
   end
 
-rescue Errno::ECONNREFUSED => exc
-    $stderr.puts "
-      WARNING!
-      WARNING! schleuder-api-daemon is not running!
-      WARNING! '#{exc}'
-      WARNING!
-      WARNING! This schleuder-web will be very useless without schleuder-api-daemon!
-      WARNING!
-      ".gsub(/^\s*/, '')
 end
-
