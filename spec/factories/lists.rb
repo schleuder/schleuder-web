8838c63e08f18eb55ee86cb779ac57be1ef1b24a
Nina Siessegger <n.siessegger@zeromail.org>
2018-02-10 20:07:47 +0100

    Add spec for superadmin editing subscription

---
+++ spec/factories/lists.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :list do
+    email "list@example.org"
+
+    trait :with_one_subscription do
+      after(:build) do |list|
+        create(:subscription)
+      end
+    end
+    factory :list_with_one_subscription, traits: [:with_one_subscription]
+  end
+end
