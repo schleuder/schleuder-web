a46310733e914d63fde70fe049f6e209080bbccd
Nina Siessegger <n.siessegger@zeromail.org>
2018-02-10 20:40:40 +0100

    Remove unused method from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,13 +14,3 @@ end
 require 'webmock/rspec'
 WebMock.disable_net_connect!
 
-def log_in_as_super_admin
-  user = create(:account, email: 'root@localhost', password: 'password')
-  session[:cookietest] = true
-  session[:current_account_id] = user.id
-  session[:login_expires_at] = 30.minutes.from_now.to_s
-end
-
-def log_out
-  session.clear
-end
