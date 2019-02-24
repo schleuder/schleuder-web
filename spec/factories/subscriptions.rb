50824f6d360978f313a0de8f98dcc4dfdb0b48d7
ng <ng@immerda.ch>
2019-02-24 20:26:11 +0100

    fix factories, so they work with factory_bot 5

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :subscription do
     sequence(:email) {|n| "user#{n}@example.org" }
-    fingerprint "129A74AD5317457F9E502844A39C61B32003A8D8"
-    admin false
-    delivery_enabled true
-    list_id 1
+    fingerprint { "129A74AD5317457F9E502844A39C61B32003A8D8" }
+    admin { false }
+    delivery_enabled { true }
+    list_id { 1 }
   end
 end
