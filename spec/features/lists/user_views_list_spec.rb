623e17f3e7c1dd80840d57187bc99d6ace8d643f
Laurent Arnoud <laurent@spkdev.net>
2020-01-18 18:08:45 +0100

    Fix rendering error message when list.key is not available
    When a key is not found ActiveResource::ResourceNotFound is raised as a
    result a 404 is rendered because of `rescue_from` in the
    application_controller.
    With this patch we are returning nil so we can show the error defined on
    _list_menu to the browser.

---
+++ spec/features/lists/user_views_list_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+feature "User views a list" do
+  scenario "when the key is found but no admins present" do
+    email = "subscription1@example.org"
+    account = create(:account, email: email)
+    subscription1 = create(:subscription, email: email)
+    list = create(:list, email: email, subscriptions: [subscription1])
+    stub_request(
+      :get,
+      "https://localhost:4443/keys/59C71FB38AEE22E091C78259D06350440F759BD3.json?list_id=#{list.id}"
+    ).to_return(status: 200, body: File.read(File.join(fixture_path, "key.json")))
+
+    sign_in(account)
+    visit(list_subscriptions_path(list))
+    expect(page).to have_content('This list is not functional!')
+    expect(page).to have_content("It needs at least one list-admin!")
+    WebMock.reset!
+  end
+
+  scenario "when the key is not found it show the error message" do
+    email = "subscription1@example.org"
+    account = create(:account, email: email)
+    subscription1 = create(:subscription, email: email, admin: true)
+    list = create(:list, email: email, subscriptions: [subscription1])
+    stub_request(
+      :get,
+      "https://localhost:4443/keys/59C71FB38AEE22E091C78259D06350440F759BD3.json?list_id=#{list.id}"
+    ).to_return(status: 404, body: "[]")
+
+    sign_in(account)
+    visit(list_subscriptions_path(list))
+    expect(page).to have_content('This list is not functional!')
+    expect(page).to have_content("No list-key present for #{list.email}")
+    WebMock.reset!
+  end
+end
