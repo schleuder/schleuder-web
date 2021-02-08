a07295af348006e94c9d0da8fb6e8b2f5315dc44
paz <paz@nadir.org>
2021-02-08 20:03:45 +0100

    Fix a deprecation.

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -2,7 +2,7 @@ class EmailValidator <  ActiveModel::EachValidator
   EMAIL_REGEXP = /\A.+@[[:alnum:]_.-]+\z/i
   def validate_each(record, attribute, value)
     unless value =~ EMAIL_REGEXP
-      record.errors[attribute] << (options[:message] || I18n.t("errors.invalid_email"))
+      record.errors.add(attribute, options[:message] || I18n.t("errors.invalid_email"))
     end
   end
 end
