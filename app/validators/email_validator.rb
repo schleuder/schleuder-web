6dae13f2436a487ae4f2a55c4a43816d6b945c22
ng <ng@immerda.ch>
2021-03-18 13:21:31 +0100

    proper regexp for email validation

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,7 +1,6 @@
 class EmailValidator <  ActiveModel::EachValidator
-  EMAIL_REGEXP = /\A.+@[[:alnum:]_.-]+\z/i
   def validate_each(record, attribute, value)
-    unless value =~ EMAIL_REGEXP
+    unless value =~ URI::MailTo::EMAIL_REGEXP
       record.errors.add(attribute, options[:message] || I18n.t("errors.invalid_email"))
     end
   end
