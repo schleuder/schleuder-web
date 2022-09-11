cb6c4f405267a6cace8452286f7df7b3357f3a79
paz <paz@schleuder.org>
2022-09-11 16:25:17 +0200

    Fix ruby 3.1: Allow aliases in YAML
    Version 4.0 of Psych, the YAML parsing library, uses a "safe" mode by
    default now, which disallows aliases (among other things)
    <https://www.ctrl.blog/entry/ruby-psych4.html>.
    We fix that by manually allowing aliases.

--- config/initializers/01_config_erb.rb
+++ config/initializers/01_config_erb.rb
@@ -2,7 +2,12 @@ module Squire
   module Parser
     module YAML
       def self.parse(path)
-        ::YAML::load(ERB.new(File.read(path)).result)
+        rendered_yaml = ERB.new(File.read(path)).result
+        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.1.0')
+          ::YAML::load(rendered_yaml, aliases: true)
+        else
+          ::YAML::load(rendered_yaml)
+        end
       end
     end
   end
