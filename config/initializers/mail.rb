398be1dab1f9d8aff655d7377f2292b1c5a7f3ce
Nina Siessegger <n.siessegger@zeromail.org>
2022-09-11 16:57:34 +0200

    Upgrade to rails 7
    From rails 7 on zeitwerk is used for autoloading.
    
    https://guides.rubyonrails.org/classic_to_zeitwerk_howto.html
    
    This lead to an issue, where Conf was not being autoloaded when used
    from an initializers. The occurences are now wrapped with a call of
    ` Rails.application.reloader.to_prepare do` to load the class in
    question.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,13 @@
-Rails.application.configure do
-  config.action_mailer.delivery_method = Conf.delivery_method.to_sym
+Rails.application.reloader.to_prepare do
+  Rails.application.config.action_mailer.delivery_method = Conf.delivery_method.to_sym
   # Assign only the appropriate settings, assigning the others breaks delivery.
   case Conf.delivery_method.to_sym
   when :smtp
-    config.action_mailer.smtp_settings = Conf.smtp_settings.to_hash
+    Rails.application.config.action_mailer.smtp_settings = Conf.smtp_settings.to_hash
   when :sendmail
-    config.action_mailer.sendmail_settings = Conf.sendmail_settings.to_hash
+    Rails.application.config.action_mailer.sendmail_settings = Conf.sendmail_settings.to_hash
   when :file
-    config.action_mailer.file_settings = Conf.file_settings.to_hash
+    Rails.application.config.action_mailer.file_settings = Conf.file_settings.to_hash
   end
-  config.action_mailer.raise_delivery_errors = true
+  Rails.application.config.action_mailer.raise_delivery_errors = true
 end
