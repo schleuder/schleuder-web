283f32c9d236194bce6bc2b3a89fadad1dc721c7
paz <paz@schleuder.org>
2024-03-12 18:43:27 +0100

    Use Rails logger for errors with schleuder-api-daemon

--- config/initializers/zzz_check_api_version.rb
+++ config/initializers/zzz_check_api_version.rb
@@ -6,47 +6,14 @@ if API_REQUIRED
     begin
       found_api_version = Base.api_version
       if Gem::Version.new(found_api_version) < Gem::Version.new(REQUIRED_API_VERSION)
-        $stderr.puts "
-        WARNING!
-        WARNING! The version of the running schleuder-api-daemon is too old!
-        WARNING!
-        WARNING! This schleuder-web will run into problems if you don't fix that!
-        WARNING! Required API-version: #{REQUIRED_API_VERSION}
-        WARNING! API-version running:  #{found_api_version}
-        WARNING!
-        ".gsub(/^\s*/, '')
+        Rails.logger.error "The version of the running schleuder-api-daemon is too old! This schleuder-web will run into problems if you don't fix that! Required API-version: #{REQUIRED_API_VERSION}. API-version running:  #{found_api_version}."
       end
-
     rescue Errno::ECONNREFUSED => exc
-      $stderr.puts "
-        WARNING!
-        WARNING! schleuder-api-daemon is not running!
-        WARNING! '#{exc}'
-        WARNING!
-        WARNING! This schleuder-web will be very useless without schleuder-api-daemon!
-        WARNING!
-      ".gsub(/^\s*/, '')
-
+      Rails.logger.error "schleuder-api-daemon is not running! '#{exc}'. This schleuder-web will be very useless without schleuder-api-daemon!"
     rescue ActiveResource::BadRequest => exc
-      $stderr.puts "
-        WARNING!
-        WARNING! schleuder-api-daemon returned HTTP status code 400.
-        WARNING! '#{exc}'
-        WARNING!
-        WARNING! Please have a look at its output to find out about the causal problem.
-        WARNING!
-        WARNING! This schleuder-web will be very useless without schleuder-api-daemon!
-        WARNING!
-      ".gsub(/^\s*/, '')
-
+      Rails.logger.error "schleuder-api-daemon returned HTTP status code 400: '#{exc}'. Please have a look at its output to find out about the causal problem. This schleuder-web will be very useless without schleuder-api-daemon!"
     rescue StandardError => exc
-      $stderr.puts "
-        WARNING!
-        WARNING! An error occurred while contacting schleuder-api-daemon:
-        WARNING! '#{exc}'
-        WARNING!
-      ".gsub(/^\s*/, '')
-
+      Rails.logger.error "An unknown error occurred while contacting schleuder-api-daemon: #{exc}"
     end
   end
 end
